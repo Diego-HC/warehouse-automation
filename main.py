d720a028eeb88182278665ba9f4ceae136e9757f
UmActually <89704397+UmActually@users.noreply.github.com>
2023-08-21 16:48:06 -0600

    se vienen cositas

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Warehouse Automation System
+===========================
+
+Hola
+
+Autores
+-------
+
+- Leonardo Corona Garza
+- Diego Eduardo Hernández Cadena
+- Andrea Guadalupe Badillo Ibarra
+- Bella Elisabet Perales Meléndez y Alcocer
+- Alexa Jimena Ramírez Ortiz
+"""
+
+print("Hello, World!")
